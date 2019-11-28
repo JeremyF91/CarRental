c7447131bed711488ca27e94b7492e6273e7a53a
Amel-Benaza <45474676+Amel-Benaza@users.noreply.github.com>
2019-11-28 17:54:57 +0100

    Test commit

--- src/main/java/com/example/CarRental/CarController.java
+++ src/main/java/com/example/CarRental/CarController.java
@@ -2,7 +2,6 @@ package com.example.CarRental;
 
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
@@ -31,57 +30,5 @@ public class CarController {
         cars.add(car);
     }
 
-/*
-    @RequestMapping(value = "/cars/plateNumber", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Car aCar(@PathVariable("plateNumber") String plateNumber) throws Exception{
-        System.out.println(plateNumber);
-        for(Car Car : cars) {
-            if(Car.getPlateNumber().contentEquals(plateNumber)) { return Car; }
-        }
-        return null;
-    }
-
-    @RequestMapping(value = "/rents/rented", method = RequestMethod.PUT)
-    public void rentAndGetBack(@PathVariable("plateNumber") String plateNumber,
-                               @RequestParam(value="rented", required = true)boolean rented,
-                               @RequestBody(required = false) Date date){
-
-        for(Car car : cars) {
-            if(car.getPlateNumber().equals(plateNumber)) {
-
-                if (rented == true) {
-
-                    car.setRented(true);
-
-                    for(Rent rent: rents) {
-                        if (rent.getPlateNumber().equals(plateNumber))
-
-                            rent.setBeginRent(rent.getBeginRent());
-                            rent.setEndRent(rent.getEndRent());
-                            System.out.println("Car Rented :" + car);
-                    }
-
-                }
-                else if (rented == false) {
-                    car.setRented(false);
-
-                    for(Rent rent: rents) {
-                        if (rent.getPlateNumber().equals(plateNumber))
-
-                            rent.setBeginRent(null);
-                            rent.setEndRent(null);
-                            System.out.println("Car unrented :" + car);
-                    }
-
-                }
-            }
-        }
-    }
-
-
-
-*/
 
 }
